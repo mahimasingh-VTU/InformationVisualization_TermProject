6af10883ab6e21a6c4af22c348ddd48d5b48166f
Mahima Singh <123506690+mahimasingh-VTU@users.noreply.github.com>
2024-06-24 10:22:28 -0400

    Adding the static graphs

--- data_cleaner.py
+++ data_cleaner.py
@@ -5,9 +5,6 @@ class DataCleaner:
         self.df = df
 
     def clean_data(self):
-        # drop vin column
-        self.df = self.df.drop(columns=['vin'])
-
         # Handle missing values
         self.df = self.df.dropna(subset=['make','trim','body','interior','transmission','color', 'model', 'sellingprice', 'mmr', 'condition', 'odometer'])
 
@@ -26,25 +23,7 @@ class DataCleaner:
         self.df['transmission'] = self.df['transmission'].str.lower()
         self.df['color'] = self.df['color'].str.lower()
         self.df['interior'] = self.df['interior'].str.lower()
-        self.df['state'] = self.df['state'].str.upper()
+        self.df['state'] = self.df['state'].str.lower()
         self.df['seller'] = self.df['seller'].str.lower()
 
-        # Remove rows with unrealistic values
-        self.df = self.df[self.df['color'] != '—']
-        self.df = self.df[self.df['interior'] != '—']
-
-        # Convert date columns to datetime
-        self.df['saledate'] = pd.to_datetime(self.df['saledate'], utc=True, format='mixed').dt.date
-
-        # Clean the body column
-        self.df['body'] = self.df['body'].str.replace(r'.*sedan.*', 'sedan', regex=True)
-        self.df['body'] = self.df['body'].str.replace(r'.*convertible.*', 'convertible', regex=True)
-        self.df['body'] = self.df['body'].str.replace(r'.*coupe.*', 'coupe', regex=True)
-        self.df['body'] = self.df['body'].str.replace(r'.*van.*', 'van', regex=True)
-        self.df['body'] = self.df['body'].str.replace(r'.*cab.*', 'pickup', regex=True)
-        self.df['body'] = self.df['body'].str.replace(r'.*wagon.*', 'wagon', regex=True)
-        self.df['body'] = self.df['body'].str.replace(r'.*supercrew.*', 'van', regex=True)
-        self.df['body'] = self.df['body'].str.replace('koup', 'coupe')
-
-        print(self.df.info())
         return self.df
